79054c58baa19168070864b5e03f1e2bc8e4df42
Zarina <kuzhakova-zarina@mail.ru>
2022-12-02 14:53:11 +0300

    Initial commit

---
+++ src/main/java/com/example/springsingleproject/SpringSingleProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsingleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSingleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSingleProjectApplication.class, args);
+        System.out.println("Happiness can be found even in the darkest of times, " +
+                "if one only remembers to turn on the light");
+    }
+
+}
