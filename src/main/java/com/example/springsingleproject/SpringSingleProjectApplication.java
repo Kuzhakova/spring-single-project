4df8189cb702af67d3dab92f4d9949f674f06b1a
Zarina <kuzhakova-zarina@mail.ru>
2022-12-05 13:43:20 +0300

    Update SpringSingleProjectApplication.java

--- src/main/java/com/example/springsingleproject/SpringSingleProjectApplication.java
+++ src/main/java/com/example/springsingleproject/SpringSingleProjectApplication.java
@@ -9,7 +9,7 @@ public class SpringSingleProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSingleProjectApplication.class, args);
         System.out.println("Happiness can be found even in the darkest of times, " +
-                "if one only remembers to turn on the light");
+                "if one only remembers to turn on the light (c) Albus Dumbledore");
     }
 
 }
